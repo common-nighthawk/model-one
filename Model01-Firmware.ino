36a31e4154c92e1f936e2a2d469da6e88863ecc3
Daniel Deutsch <daniel.deutsch@outreach.io>
2019-10-10 22:10:24 -0700

    First full pass

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -89,6 +89,7 @@
   */
 
 enum { MACRO_VERSION_INFO,
+       MACRO_UNDERSCORE,
        MACRO_ANY
      };
 
@@ -174,18 +175,23 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+  (Key_Backtick,    Key_Escape,   ___,   ___,   ___,   ___,   Key_PageUp,
+   Key_Tab,         Key_Q,        Key_W, Key_E, Key_R, Key_T, Key_PageDown,
+   Key_LeftShift,   Key_A,        Key_S, Key_D, Key_F, Key_G,
+   Key_LeftAlt,     Key_Z,        Key_X, Key_C, Key_V, Key_B, Key_LeftArrow,
+
+   Key_Spacebar, M(MACRO_UNDERSCORE), Key_LeftControl, Key_LEDEffectNext,
+
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+
+   Key_UpArrow,    ___,   ___,   ___,       ___,         ___,       Key_Backspace,
+   Key_DownArrow,  Key_Y, Key_U, Key_I,     Key_O,       Key_P,     Key_Semicolon,
+                   Key_H, Key_J, Key_K,     Key_L,       Key_Quote, Key_RightShift,
+   Key_RightArrow, Key_N, Key_M, Key_Comma, Key_Period,  Key_Slash, Key_RightAlt,
+
+   Key_LeftGui, Key_RightControl, Key_Enter, Key_Spacebar,
+
    ShiftToLayer(FUNCTION)),
 
 #elif defined (PRIMARY_KEYMAP_DVORAK)
@@ -263,19 +269,23 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
-   ___,
+  (___, ___,   ___,   ___,   ___,              ___,                  ___,
+   ___, Key_Q, Key_W, Key_E, Key_LeftBracket,  Key_KeypadLeftParen,  Key_KeypadLeftCurlyBrace,
+        ___,   Key_A, Key_S, Key_RightBracket, Key_KeypadRightParen, Key_KeypadRightCurlyBrace,
+   ___, Key_Z, Key_X, Key_C, Key_V, Key_B, ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
-   ___)
+   ___, ___, ___, ___,
+
+   ShiftToLayer(FUNCTION),
+
+   ___, ___, ___,   ___,   ___,   ___,   ___,
+   ___, ___, Key_7, Key_8, Key_9, Key_0, ___,
+        ___, Key_4, Key_5, Key_6, ___,   ___,
+   ___, ___, Key_1, Key_2, Key_3, ___,   ___,
+
+   ___, ___, ___, ___,
+
+   ___),
 ) // KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
@@ -290,6 +300,12 @@ static void versionInfoMacro(uint8_t keyState) {
   if (keyToggledOn(keyState)) {
     Macros.type(PSTR("Keyboardio Model 01 - Kaleidoscope "));
     Macros.type(PSTR(BUILD_INFORMATION));
+  }
+}
+
+static void underscoreMacro(uint8_t keyState) {
+  if (keyToggledOn(keyState)) {
+    Macros.type(PSTR("_"));
   }
 }
 
@@ -331,6 +347,10 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 
   case MACRO_VERSION_INFO:
     versionInfoMacro(keyState);
+    break;
+
+  case MACRO_UNDERSCORE:
+    underscoreMacro(keyState);
     break;
 
   case MACRO_ANY:
